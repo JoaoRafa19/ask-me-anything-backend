f5409ee3338f3f3b66df074bb9a882cd05f1a15c
JoaoRafa19 <joaopedrorafael19@gmail.com>
2024-08-05 15:24:01 -0300

    fim aula 1

--- internal/store/pgstore/queries/queries.sql
+++ internal/store/pgstore/queries/queries.sql
@@ -1,9 +1,75 @@
 -- name: GetRoom :one
-select "id", "theme" 
-from rooms 
-where id = $1;
+SELECT
+    "id",
+    "theme"
+FROM
+    rooms
+WHERE
+    id = $1;
 
 -- name: GetRooms :many
-select
-"id", "theme"
-from rooms;
+SELECT
+    "id",
+    "theme"
+FROM
+    rooms;
+
+-- name: InsertRoom :one
+INSERT INTO
+    rooms ("theme")
+VALUES
+    ($1) RETURNING "id";
+
+-- name: GetMessage :one
+SELECT
+    "id",
+    "room_id",
+    "message",
+    "reaction_count",
+    "awnsered"
+FROM
+    messages
+WHERE
+    id = $1;
+
+-- name: GetRoomMessages :many
+SELECT
+    "id",
+    "room_id",
+    "message",
+    "reaction_count",
+    "awnsered"
+FROM
+    messages
+WHERE
+    room_id = $1;
+
+-- name: InsertMessage :one
+INSERT INTO
+    messages ("room_id", "message")
+VALUES
+    ($1, $2) RETURNING "id";
+
+-- name: ReactToMessage :one
+UPDATE
+    messages
+SET
+    reaction_count = reaction_count + 1
+WHERE
+    id = $1 RETURNING reaction_count;
+
+-- name: RemoveReactionFromMessage :one
+UPDATE
+    messages
+SET
+    reaction_count = reaction_count - 1
+WHERE
+    id = $1 RETURNING reaction_count;
+
+-- name: MarkMessageAsAwnsered :exec
+UPDATE
+    messages
+SET
+    awnsered = true
+WHERE
+    id = $1;
